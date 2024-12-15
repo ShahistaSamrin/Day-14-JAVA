9c0f20aa25c1d840b38a8ae891bbedef3096de7e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-15 12:04:27 +0530

    Add files via upload

---
+++ Sleepmethod.java
@@ -0,0 +1,16 @@
+/*  Thread Started..//after sometime it prints
+Thread resumed  */
+public class Sleepmethod{
+    public static void main(String args[]){
+        System.out.println("Thread Started..");
+        try
+        {
+            Thread.sleep(5000);
+        }catch(InterruptedException e)
+        {
+            System.out.println("Interrupted");
+        }
+        System.out.println("Thread resumed");
+        
+    }
+}
